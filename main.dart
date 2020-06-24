fb590ba6d23675c995ed5e42fea3446242dbf721
Ricardo2001zg <miao@ricardo2001zg.moe>
2020-06-24 22:12:18 +0800

    README update

---
+++ main.dart
@@ -0,0 +1,23 @@
+//With None License
+//Powered by Ricardo2001zg
+
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter/material.dart';
+
+class AppMain extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: [
+          const Locale.fromSubtags(languageCode: 'zh', scriptCode: 'Hans', countryCode: 'CN'),// Chinese *See Advanced Locales below*
+        ],
+        title: '不语惜言',
+        home:Text('1'),
+    );
+  }
+}
