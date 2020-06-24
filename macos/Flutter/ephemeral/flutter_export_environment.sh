7320521c64f09faa8b511ed303ff9435f45505e9
Ricardo2001zg <miao@ricardo2001zg.moe>
2020-06-24 19:28:52 +0800

    Initialization

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\miao\project\application\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\miao\project\bu_yu_xi_yan"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_FRAMEWORK_DIR=C:\Users\miao\project\application\src\flutter\bin\cache\artifacts\engine\darwin-x64"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
