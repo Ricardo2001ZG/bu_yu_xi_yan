b6b86b022c0f577ac0ed6370bc25b5850509724a
Ricardo2001zg <miao@ricardo2001zg.moe>
2020-06-25 13:31:51 +0800

    status bar update

--- lib/main.dart
+++ lib/main.dart
@@ -17,9 +17,57 @@ class AppMain extends StatelessWidget {
           const Locale.fromSubtags(languageCode: 'zh', scriptCode: 'Hans', countryCode: 'CN'),// Chinese *See Advanced Locales below*
         ],
         title: '不语惜言',
-        home:Text('1'),
+        home:Scaffold(
+
+          body: Row(children: [
+            /*
+            主界面的三个基础Container，将页面划分为左中右三部分。
+            最左侧为状态栏，中间为通知栏，右侧为信息栏。
+            其中，左侧固定宽度50，中间综合宽度260，右侧随系统填充。
+            左侧固定颜色64b5f6(300)。
+            */
+            Container(
+              width: 50,
+              color: Color(0xff64b5f6),
+              child: Column(children: [
+                /*左侧状态栏，从上到下为头像、消息图标、消息文本、文件图标、文件文本*/
+                Container(height: 25,), //空白填充
+                Container(
+                  height: 32,
+                  child: Image.network('http://127.0.0.1/user.jpg'),//头像
+                ),
+                Container(height: 20,), //空白填充
+                Container(
+                  child: Icon(Icons.message,color: Colors.white,),
+                ),
+                Container(
+                  child: Text(
+                    '消息',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Color.fromARGB(80, 255, 255, 255)),
+                  ),
+                ),
+                Container(height: 20,), //空白填充
+                Container(
+                  child: Icon(Icons.folder_open,color: Color.fromARGB(80, 255, 255, 255)),
+                ),
+                Container(
+                  child: Text(
+                    '文件',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Color.fromARGB(80, 255, 255, 255)),
+                  ),
+                ),
+              ],),
+            ),
+            Container(
+              width: 260,
+            ),
+            Expanded(child: Container(),flex: 0,),
+          ],),
+        ),
     );
   }
 }
 
-void main() {runApp(AppMain());}
+void main() => runApp(AppMain());
